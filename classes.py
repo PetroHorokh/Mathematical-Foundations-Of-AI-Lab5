29a93f46a53135471408b2872c3178077a75eeb9
Normal_Pete <petrohorokh@gmail.com>
2024-05-12 13:38:24 +0300

    third commit

--- classes.py
+++ classes.py
@@ -1,32 +1,22 @@
 class ClassTeacher:
-    def __init__(self,
-                 name,
-                 teacher):
+    def __init__(self, name, cls_teacher):
         self.name = name
-        self.teacher = teacher
+        self.teacher = cls_teacher
 
 
 class Teacher:
-    def __init__(self,
-                 name,
-                 lessons):
+    def __init__(self, name, lessons):
         self.name = name
         self.lessons = lessons
 
 
 class Lesson:
-    def __init__(self,
-                 cls="None",
-                 teacher=None,
-                 name="None",
-                 special_room_required=False,
-                 day_number=0,
-                 lesson_number=0,
-                 room=""):
+    def __init__(self, cls="None", teacher=None, name="None", special_room_required=False,
+                 day_number=0, lesson_number=0, room=""):
         self.cls = cls
         self.teacher = teacher
         self.name = name
-        self.special_room_required = special_room_required
-        self.day_number = day_number
-        self.lesson_number = lesson_number
+        self.isSpecialRoom = special_room_required
+        self.day = day_number
+        self.lessonNumber = lesson_number
         self.room = room
--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 from algorithm import *
 
-best_schedule = genetic_algorithm()
+def main():
 
-for i in range(AMOUNT_OF_CLASSES):
+    teachers = [
+        Teacher("Teacher 1", ["Math", "English", "Science", "History"]),
+        Teacher("Teacher 2", ["Math", "English", "Science", "Music"]),
+        Teacher("Teacher 3", ["Math", "English", "Science", "History"]),
+        Teacher("Teacher 4", ["PE", "Choreography"]),
+    ]
 
-    print(f"Schedule for class {classNames[i]}\n")
+    room_names = ["Room 1", "Room 2", "Room 3", "PE Room", "Dance Room", "Music Room"]
 
-    for j in range(AMOUNT_OF_DAYS):
+    class_names = ["Class A", "Class B", "Class C"]
 
-        print(f"{days[j]}\n")
+    lesson_names = [["Math", False, "None"], ["English", False, "None"], ["Science", False, "None"],
+                    ["History", False, "None"], ["PE", True, "PE Room"], ["Choreography", True, "Dance Room"],
+                    ["Music", True, "Music Room"]]
 
-        for k in range(MAX_AMOUNT_OF_LESSONS_PER_DAY):
-            lesson = best_schedule[i][j][k]
+    fileName = "input_file"
+    # Запис даних у файл
+    write_data_to_file(fileName, AMOUNT_OF_LESSONS, AMOUNT_OF_CLASSES, AMOUNT_OF_DAYS, MAX_AMOUNT_OF_LESSONS_PER_DAY,
+                       teachers, room_names, class_names, lesson_names)
 
-            print(f"Number: {lesson.lesson_number}")
-            print(f"Subject: {lesson.name}")
-            print(f"Teacher: {lesson.teacher.name}")
+    # Читання даних з файлу
+    num_lessons, num_classes, num_days, max_lessons_per_day, teachers, room_names, class_names, lesson_names = read_data_from_file(
+        fileName)
 
-            print()
+    classes = []
+    for i in range(num_classes):
+        classes.append(ClassTeacher(class_names[i], teachers[i]))
 
-print(f"Result best fitness {fitness(best_schedule)} out of {FITNESS_LIMIT}")
+    best_schedule = genetic_algorithm(lesson_names, teachers, classes, num_lessons, num_classes,
+                                      num_days, max_lessons_per_day, room_names)
+
+    print_schedule(best_schedule, num_days, max_lessons_per_day, num_classes, class_names)
+    print(
+        f"Даний розклад має пристосованість {fitness(best_schedule, num_lessons, max_lessons_per_day)} із {FITNESS_LIMIT} можливих")
+
+
+if __name__ == "__main__":
+    main()
